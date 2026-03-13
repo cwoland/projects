75bf53ad26b5f5cb92a125324704e02c141858ba
cwoland <tkachenkoa2003@gmail.com>
2026-03-13 21:38:44 +0300

    progress Bar and shuffle

--- playlist.js
+++ playlist.js
@@ -5,10 +5,17 @@ const covers = [
     document.getElementById("album-cover3")
 ];
 
+const tracks = array.from(document.querySelectorAll(".tracklist li"));
+
 const player = document.getElementById("player");
 const customControls = document.getElementById("custom-controls");
 const prevBtn = document.getElementById("prev-btn");
 const nextBtn = document.getElementById("next-btn");
+const shuffleBtn = document.getElementById("shuffle-btn");
+
+const progressBar = document.getElementById("progress-bar");
+const currentTimeEl = document.getElementById("current-time");
+const durationEl = document.getElementById("duration");
 
 let currentLi = null;
 
@@ -66,4 +73,33 @@ prevBtn.addEventListener("click", () => {
     if (prevLi && prevLi.dataset.src) {
         prevLi.click();  // simulate click to play
     }
+});
+
+function playRandomTrack () {
+    const randomIndex = Math.floor(Math.random() * tracks.length);
+    const randomTrack = tracks[randomIndex];
+    randomTrack.click();
+}
+
+shuffleBtn.addEventListener("click", playRandomTrack);
+player.addEventListener("ended", playRandomTrack);
+
+function formatTime(time) {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+
+return minutes + ":" + String(seconds).padStart(2, "0");
+
+player.addEventListener("loadedmetadata", () => {
+    progressBar.max = player.duration;
+    durationEl.textContent = formatTime(player.duration);
+});
+
+player.addEventListener("timeupdate", () => {
+    progressBar.value = player.currentTime;
+    currentTimeEl.textContent = formatTime(player.currentTime);
+});
+
+player.addEventListener("input", () => {
+    player.currentTime = progressBar.value;
 });
